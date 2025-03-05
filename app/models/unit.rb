e439e69b182f01cfceef35a6f0880b789b43f4b3
rockwell rice <rockwellrice@MacBookPro.lan>
2025-03-05 14:00:52 -0600

    Adds modela validations for has_many - Development (GREEN) * Adds has_many for Unit on Tenants *  Adds has_many for Unit on Leases modified:   app/models/unit.rb

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -1,6 +1,9 @@
 class Unit < ApplicationRecord
   enum :floor_plan, {:studio=>0, :suite=>1, :two_bedroom=>2, :three_bedroom=>3}
 
+  has_many :leases, dependent: :destroy
+  has_many :tenants, through: :leases
+
   validates :unit_number, presence: true
   validates :floor_plan, presence: true
 end
