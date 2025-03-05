bad6119b8996993f3df13e3bfc4754c737e0af29
rockwell rice <rockwellrice@MacBookPro.lan>
2025-03-05 13:00:48 -0600

    Adds validations to model - Development (Green) * Adds presence validations to model for required fields (what I assume would be required fields for the data I should say) modified:   app/models/unit.rb

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -1,2 +1,6 @@
 class Unit < ApplicationRecord
+  enum :floor_plan, {:studio=>0, :suite=>1, :two_bedroom=>2, :three_bedroom=>3}
+
+  validates :unit_number, presence: true
+  validates :floor_plan, presence: true
 end
