a1d3dc8a348aff9dabc06c4ab0481d4681d279a7
rockwell rice <rockwellrice@MacBookPro.lan>
2025-03-05 13:59:40 -0600

    Adds assumed relationships for Unit has_many Tenants and Leases - Development (RED) * Adds test for has_many relationships in Unit model (assumptions for exercise) modified:   spec/models/unit_spec.rb

--- spec/models/unit_spec.rb
+++ spec/models/unit_spec.rb
@@ -3,6 +3,9 @@ require 'rails_helper'
 RSpec.describe Unit, type: :model do
   subject { described_class.new }
 
+  let(:unit) { Unit.create(unit_number: '10', floor_plan: :studio) }
+  let(:tenant) { Tenant.create(name: 'John Doe') }
+
   it 'requires a unit_name' do
     subject.unit_number = nil
     subject.floor_plan = 1
@@ -19,5 +22,15 @@ RSpec.describe Unit, type: :model do
     subject.unit_number = '1A'
     subject.floor_plan = 1
     expect(subject).to be_valid
+  end
+
+  it 'can have many Leases' do
+    lease = Lease.create(unit: unit, tenant: tenant, move_in: '2023-08-01', move_out: '2024-08-31')
+    expect(unit.leases).to include(lease)
+  end
+
+  it 'can have many Tenants' do
+    lease = Lease.create(unit: unit, tenant: tenant, move_in: '2023-08-01', move_out: '2024-08-31')
+    expect(unit.tenants).to include(tenant)
   end
 end
